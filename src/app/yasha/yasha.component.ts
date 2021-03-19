55234e9eb206104e32616ecb864a3432e8c7c436
Salman Mujtaba <salmanmujtaba@Salmans-MacBook-Pro.local>
2021-03-19 02:41:54 -0400

    add yasha

---
+++ src/app/yasha/yasha.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yasha',
+  templateUrl: './yasha.component.html',
+  styleUrls: ['./yasha.component.scss']
+})
+export class YashaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
