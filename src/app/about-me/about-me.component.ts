338d4b1df48222e1d4e31cc0c1297ac9fd64131c
Salman Mujtaba <salmanmujtaba@macbook.local>
2022-06-13 17:51:35 -0400

    Add typewriter animation and enable it on scroll

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild } from "@angular/core";
 
 import { Redirect } from "./../../services/redirect.service";
 
@@ -8,8 +8,44 @@ import { Redirect } from "./../../services/redirect.service";
   styleUrls: ["./about-me.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutMeComponent {
-  constructor(private redirects: Redirect) {}
+export class AboutMeComponent implements AfterViewInit {
+  @ViewChild("animatedText") animatedText: ElementRef;
+  constructor(private redirects: Redirect) {
+
+  }
+
+  ngAfterViewInit() {
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.7
+    };
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.animatedText.nativeElement.classList.add("text", "animation");
+        } else {
+          this.animatedText.nativeElement.classList.remove("text", "animation");
+        }
+      }), options
+    });
+    observer.observe(document.getElementById('about'));
+  }
+
+  callbackFunc(entries, observer) {
+    entries.forEach(entry => {
+      this.toggleAnimation(entry.isIntersecting);
+    });
+  }
+
+  toggleAnimation(isVisible): void {
+    if (isVisible) {
+      this.animatedText.nativeElement.classList.add("text animation");
+    } else {
+      this.animatedText.nativeElement.classList.remove("text animation");
+    }
+  }
+
 
   goToGithub(): void {
     this.redirects.goToGithub();
