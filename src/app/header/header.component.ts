8311c1794704f681721acb98f5c7d4594328473f
Salman Mujtaba <salmanmujtaba@macbook.local>
2022-10-10 01:29:13 -0400

    Headerbar fix for the mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { AfterViewInit, ChangeDetectionStrategy, Component, Inject } from "@angular/core";
 
 @Component({
   selector: "app-header",
@@ -6,8 +7,20 @@ import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
   styleUrls: ["./header.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
+export class HeaderComponent implements AfterViewInit {
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
-  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    let navbar = document.getElementById("navbarCollapse") as HTMLElement;
+    let links = navbar.firstElementChild as HTMLElement;
+    links?.classList.add("header-collapsed-navbar");
+
+    document.getElementsByClassName("navbar-toggler ")[0]?.addEventListener("click", (event) => {
+      if (navbar?.style?.height === '0px') {
+        links?.classList.remove("header-collapsed-navbar");
+      } else {
+        links?.classList.add("header-collapsed-navbar");
+      }
+    })
+  }
 }
