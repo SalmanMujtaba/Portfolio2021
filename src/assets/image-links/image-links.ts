ee307f44353fb6bc11b38123d04931089583c73d
Salman Mujtaba <salmanmujtaba@macbook.local>
2022-10-22 02:26:36 -0400

    Remove unwanted SVGs. Style cleanup.

--- src/assets/image-links/image-links.ts
+++ src/assets/image-links/image-links.ts
@@ -1,3 +1,3 @@
 export const imageLinks: Map<string, string> = new Map(
-  [['ABOUT_ME', 'https://sat02pap002files.storage.live.com/y4mUjKfSDh_PY1o5ovaFL5b3r6Iszl7w2nAtGOa3eIfsWewbrvHTXWKZxpZgR4ezFkIaSzpvle2NMhxjZsRP2rdOemwG4RyZNnymTMrohE_0qA0RgC4mM-UB3W0vmgl8yN8rBRLEUjve-DZdPuLbbRA6A_WmIy7N4LxO3v1q8bNaEMRmeZMMGSEs2EyuGC-eaWr?width=4032&height=3024&cropmode=none']
+  [['ABOUT_ME', 'https://sat02pap002files.storage.live.com/y4mUjKfSDh_PY1o5ovaFL5b3r6Iszl7w2nAtGOa3eIfsWewbrvHTXWKZxpZgR4ezFkIaSzpvle2NMhxjZsRP2rdOemwG4RyZNnymTMrohE_0qA0RgC4mM-UB3W0vmgl8yN8rBRLEUjve-DZdPuLbbRA6A_WmIy7N4LxO3v1q8bNaEMRmeZMMGSEs2EyuGC-eaWr?width=4032&height=3024&cropmode=none'],
   ]);
