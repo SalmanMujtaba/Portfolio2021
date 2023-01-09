31e5a1b296da65c3b680e58a7c731280f08c1a9d
Salman Mujtaba <salmanmujtaba@macbook.local>
2023-01-08 19:11:26 -0500

    Fix navbar in mobile.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,16 +11,19 @@ export class HeaderComponent implements AfterViewInit {
   constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngAfterViewInit(): void {
-    // let navbar = document.getElementById("navbarCollapse") as HTMLElement;
-    // let links = navbar.firstElementChild as HTMLElement;
-    // links?.classList.add("header-collapsed-navbar");
+    let navbar = document.getElementById("navbarCollapse") as HTMLElement;
+    if (navbar) {
+      navbar.style.height = "0px";
+    }
 
-    // document.getElementsByClassName("navbar-toggler ")[0]?.addEventListener("click", (event) => {
-    //   if (navbar?.style?.height === '0px') {
-    //     links?.classList.remove("header-collapsed-navbar");
-    //   } else {
-    //     links?.classList.add("header-collapsed-navbar");
-    //   }
-    // });
+    document.getElementsByClassName("navbar-toggler")[0]?.addEventListener("click", (event) => {
+      if (navbar?.style?.height !== '0px') {
+        setTimeout(() => {
+          navbar.style.display = "none";
+        }, 301);
+      } else {
+        navbar.style.display = "unset";
+      }
+    });
   }
 }
